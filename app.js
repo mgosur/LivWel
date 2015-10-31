a9724c9f91c47ee4e17a20644533f360fb878dc0
Michael Vander Lugt <mvlugt@umich.edu>
2015-10-31 12:37:02 -0400

    morgan

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ var express = require('express');
 var passport = require('passport');
 var mongoose = require('mongoose');
 var bodyParser = require("body-parser")
+var morgan = require('morgan');
 
 var app = express();
 mongoose.connect('mongodb://localhost/LivWel')
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json()); //add more parsing as needed
+app.use(morgan('dev')); //set up file logger?
 app.use(require('./controllers'));
 
 var port = process.env.port || 8080; //listening to enviroment
