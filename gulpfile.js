e08c1af95bb09533fba0be7f7b7636102dd47db6
Michael Vander Lugt <mvlugt@umich.edu>
2015-11-21 23:51:52 -0500

    gulp file fix

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,28 @@
 var gulp = require('gulp');
 var LiveServer = require('gulp-live-server');
+var browserSync = require('browser-sync');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var sourceStream = require('vinyl-source-stream');
 
 gulp.task('live-server', function() {
-	var server = new LiveServer('server/main.js');
+	var server = LiveServer('./app/server.js');
 	server.start();
+})
+gulp.task('bundle', function() {
+	return browserify({
+		entries: 'public/main.jsx',
+		debug: true,
+	})
+	.transform(reactify)
+	.bundle()
+	.pipe(sourceStream('build.js'))
+	.pipe(gulp.dest('./.dist'));
+})
+//browser sync allowing for stringing together of 
+gulp.task('serve', ['bundle','live-server'], function() {
+	browserSync.init(null, {
+		proxy:"http://localhost:8888",
+		port: 9001
+	})
 })
