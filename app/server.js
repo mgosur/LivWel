7df4088c9668b1f2040f750254c758117ddfb5c9
Michael Vander Lugt <mvlugt@umich.edu>
2015-11-22 00:18:32 -0500

    routing fixed

--- app/server.js
+++ app/server.js
@@ -1,12 +1,15 @@
 var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
 var app = express();
+
  
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/../.dist'));
-
-app.get('/', function(req, res) {
-	res.render('../public/index');
-})
+app.use(bodyParser.json()); //allows request parsing
+app.use(morgan('dev')); //request logging to terminal
+app.use(require('./controllers')); //load in controllers
 
 var port = process.env.port || 8888; //listening to enviroment
 app.listen(port, function() {
